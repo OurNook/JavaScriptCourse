05203bf8812bfa69bb9cd6c1570d44652e830435
Jamie Mills <jamie@ournook.uk>
2026-03-25 12:57:28 +0000

    update

---
+++ 02-arrays_and_objects/02_array_methods/script.js
@@ -0,0 +1,23 @@
+let x;
+
+const arr = [34, 55, 95, 20, 15];
+
+// arr.push(100); // add to end
+// arr.pop(); // remove last element
+// arr.unshift(99); // add to start
+// arr.shift(); // remove first element
+// arr.reverse(); // reverse array
+
+x = arr.includes(20) // Is value included as an element
+
+x = arr.indexOf(34) // Identify Elements Index Number
+
+x = arr.slice(1, 4) // Slice array to only show between the index numbers
+
+// x = arr.splice(1, 3) // Shows element from first number, second number how many
+
+//x = arr.splice(3, 1)
+
+x = arr.splice(1, 4).reverse().toString().charAt(0);
+
+console.log(x);
