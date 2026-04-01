4bfd8b864b25c85cd7a4e9478b58481b6c078ed3
Jamie Mills <jamie@ournook.uk>
2026-04-01 16:44:56 +0100

    update

---
+++ 03_function_scope_execution/04_block_scope/script.js
@@ -0,0 +1,26 @@
+const x = 100;
+const foo = 1;
+var bar = 2;
+
+if (true) {
+  const y = 200;
+}
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+console.log(c);
+
+function run() {
+  var d = 100;
+  console.log(d);
+}
+
+run();
